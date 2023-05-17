ee721b61b611dcdf431dedfed36e16540e04c5de
Carolina Ramirez <solcaro_8717@hotmail.com>
2023-05-17 12:05:20 -0300

    Medio maquetado

---
+++ frontEnd/src/views/home/home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  
+  return (
+    <div>
+    <h1>NAVBAR</h1> 
+    <h2>HERO</h2>
+    <h3>MENU CON SUS RESPECTIVOS MENU Y DELIVERY </h3>
+    <h3>EVENTOS</h3>
+    <h3>SUCURSALES</h3>
+    <h3>RESEÑAS</h3>
+    <h3>FOOTER</h3>
+    </div>
+  )
+}
+export default Home
