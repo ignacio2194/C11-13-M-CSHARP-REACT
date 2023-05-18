ba9e839030fa0181de9a044909b9095fcc4d7cc0
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-05-17 21:32:08 -0300

    intentando que la api de google anda parte 1000

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,13 +1,13 @@
 
   // import Form from '../src/components/Form/Form'
 import Home from "./views/home/home.jsx"
-
+import Form from '../src/components/Form/Form.jsx'
 function App() {
   
   return (
     <div className="App">
-      {/* <Form /> */}
-      <Home />
+      <Form />
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ frontEnd/src/components/googleButton/GoogleBtn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const GoogleBtn = () => {
+  return (
+    <div>
+        <div id="g_id_onload"
+     data-client_id="226311912125-tpmbf5oplf7hbop4j78rvpj04tl7mjoe.apps.googleusercontent.com"
+     data-login_uri="http://localhost"
+     data-your_own_param_1_to_login="any_value"
+     data-your_own_param_2_to_login="any_value">
+        </div>
+    </div>
+  )
+}
+
+export default GoogleBtn
