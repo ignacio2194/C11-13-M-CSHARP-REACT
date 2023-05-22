5dc01173d3c2e7bf6c6373280f73f73cb19cebf2
Carolina Ramirez <solcaro_8717@hotmail.com>
2023-05-22 16:37:31 -0300

    7 por ciento del administrador

--- frontEnd/src/views/admin/viewUser/detailUser/detailUser.jsx
+++ frontEnd/src/views/admin/viewUser/detailUser/detailUser.jsx
@@ -1,1 +1,13 @@
 //DETAIL DEL USER CON SUS DATITOS
+
+import React from 'react';
+
+function detailUser() {
+  return (
+    <div>
+      <h1>¡Hola Tengo que mostrar el detalle de los usuarios!</h1>
+    </div>
+  );
+}
+
+export default detailUser;
