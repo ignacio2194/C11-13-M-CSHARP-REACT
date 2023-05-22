341363e7592980ef4d8a5693b3aa64a73d0883cc
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2023-05-22 17:00:47 -0300

    boton parte 1000

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
+import CssBaseline from '@mui/material/CssBaseline';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <CssBaseline />
     </BrowserRouter>
   </React.StrictMode>
 );
