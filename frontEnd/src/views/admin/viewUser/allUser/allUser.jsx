ecf9efcc61a79041a08ffcbd28b5f02658603a3d
Carolina Ramirez <solcaro_8717@hotmail.com>
2023-06-04 22:45:29 -0300

    Admin

--- frontEnd/src/views/admin/viewUser/allUser/allUser.jsx
+++ frontEnd/src/views/admin/viewUser/allUser/allUser.jsx
@@ -1,3 +1,77 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+// import SideBar from '../../dashboard/sideBar';
+// import './allUser.css';
+// import Navbar from '../../dashboard/navBar';
+
+// const AllUsers = () => {
+//   const [adminUsers, setAdminUsers] = useState([]);
+//   const [activeAdminUser, setActiveAdminUser] = useState(null); // Nuevo estado para almacenar el usuario activo
+
+//   useEffect(() => {
+//     const fetchAdminUsers = async () => {
+//       try {
+//         // Obtener todos los usuarios
+//         const response = await axios.get('https://sdlt2.azurewebsites.net/api/Account/GetAllUsers');
+//         const users = response.data;
+
+//         // Filtrar los usuarios con el rol de administrador (RoleId = "admin")
+//         const adminUsers = users.filter(user => user.RoleId === "admin");
+
+//         // Buscar el usuario activo (puedes ajustar la condición según tus necesidades)
+//         const activeUser = adminUsers.find(user => user.UserName === sessionStorage.getItem("userName"));
+//         setActiveAdminUser(activeUser);
+
+//         setAdminUsers(adminUsers);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchAdminUsers();
+//   }, []);
+
+//   return (
+//     <div>
+//       <SideBar />
+//       <Navbar />
+//       <div className="dashboard-container">
+//         <Paper className="container" style={{ backgroundColor: '#FFEBDE', padding: '20px' }}>
+//           <Typography variant="h4" component="h1" align="center">
+//             Usuarios Administradores
+//           </Typography>
+//           <List>
+//             {adminUsers.map((user) => (
+//               <ListItem key={user.Id}>
+//                 <div className="user-container">
+//                   <ListItemText
+//                     primaryTypographyProps={{ variant: 'h6' }}
+//                     primary={user.UserName}
+//                     secondary={
+//                       <React.Fragment>
+//                         <span>Email: {user.Email}</span>
+//                         <br />
+//                         <span>Username: {user.UserName}</span>
+//                       </React.Fragment>
+//                     }
+//                   />
+//                   {activeAdminUser && activeAdminUser.Id === user.Id && ( // Mostrar un indicador para el usuario activo
+//                     <span>Activo</span>
+//                   )}
+//                 </div>
+//               </ListItem>
+//             ))}
+//           </List>
+//         </Paper>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllUsers;
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
@@ -6,20 +80,30 @@ import './allUser.css';
 import Navbar from '../../dashboard/navBar';
 
 const AllUsers = () => {
-  const [allUsers, setAllUsers] = useState([]);
+  const [adminUsers, setAdminUsers] = useState([]);
+  const [activeAdminUserId, setActiveAdminUserId] = useState(null); // Nuevo estado para almacenar el ID del usuario activo
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchAdminUsers = async () => {
       try {
+        // Obtener todos los usuarios
         const response = await axios.get('https://sdlt2.azurewebsites.net/api/Account/GetAllUsers');
         const users = response.data;
-        setAllUsers(users);
+
+        // Filtrar los usuarios con el rol de administrador (RoleId = "admin")
+        const adminUsers = users.filter(user => user.RoleId === "admin");
+
+        // Buscar el ID del usuario activo (puedes ajustar la condición según tus necesidades)
+        const activeAdminUserId = adminUsers.find(user => user.UserName === sessionStorage.getItem("userName"))?.Id;
+        setActiveAdminUserId(activeAdminUserId);
+
+        setAdminUsers(adminUsers);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchUsers();
+    fetchAdminUsers();
   }, []);
 
   return (
@@ -29,10 +113,10 @@ const AllUsers = () => {
       <div className="dashboard-container">
         <Paper className="container" style={{ backgroundColor: '#FFEBDE', padding: '20px' }}>
           <Typography variant="h4" component="h1" align="center">
-            Todos los Usuarios
+            Usuarios Administradores
           </Typography>
           <List>
-            {allUsers.map((user) => (
+            {adminUsers.map((user) => (
               <ListItem key={user.Id}>
                 <div className="user-container">
                   <ListItemText
@@ -57,3 +141,4 @@ const AllUsers = () => {
 };
 
 export default AllUsers;
+
