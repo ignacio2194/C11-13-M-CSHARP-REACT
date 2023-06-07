135731055f1895d2f1500c340ee6e7b635132094
Facundo Luna <facundolunaok@gmail.com>
2023-06-06 23:52:30 -0300

    Validación envío de reservas y se agregó más info al ticket

--- frontEnd/src/views/reservas/Ticket.jsx
+++ frontEnd/src/views/reservas/Ticket.jsx
@@ -3,20 +3,42 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Box, Paper, Stack } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const Ticket2 = ({ reserva, userName }) => {
-  const fechaYHora = reserva["FechaHora"].split(" ");
+const Ticket = ({ reserva }) => {
+  const fechaYHora = reserva.FechaHora.split(" ")
   const people = reserva.Cantidad;
+  const [userName, setUserName] = useState("");
+  const [token, setToken] = useState(JSON.parse(sessionStorage.getItem("token")));
 
-  const handleClose = () => {
-    // setOpen(false);
-    // setShowTicket(false);
-    // navigate("/detalles-pedido")
-  };
+  const getUserInfo = async () => {
+    try {
+      const api = "https://sdlt2.azurewebsites.net/api/Account/UserInfo";
+      const response = await axios.get(api, {
+        headers: {
+          "Authorization": "Bearer " + token,
+          "Content-Type": "application/x-www-form-urlencoded",
+        }
+      });
+      setUserName(response.data.Email);
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+
+    return (
+      localStorage.removeItem("reserva")
+    )
+  }, [token]);
 
   return (
     <Box sx={{ width: { lg: "700px", sm: "600px", xs: "300px" }, margin: "0 auto" }}>
-      <Box onClose={handleClose} sx={{ textAlign: "center" }}>
+      <Box sx={{ textAlign: "center" }}>
         <CheckCircleIcon sx={{ backgroundColor: "#24A138", color: "custom.peachPuff", width: { lg: "128px", xs: "50px" }, height: { lg: "128px", xs: "50px" }, borderRadius: "50%", border: "none" }} />
         <Typography sx={{ fontSize: "clamp(1rem, 3vw, 2.5rem)", fontWeight: "700", margin: { lg: "16px" } }}>¡Tu reservación fue exitosa!</Typography>
         <Typography sx={{ fontSize: "clamp(1rem, 3vw, 2rem)", margin: { lg: "27px 0 34px", xs: "16px auto" } }}>Aquí te dejamos tu ticket de reservación</Typography>
@@ -24,7 +46,7 @@ const Ticket2 = ({ reserva, userName }) => {
       <Paper sx={{ backgroundColor: "#FAEECF", padding: "16px" }}>
         <Typography variant="h6" sx={{ textAlign: "center", margin: "0 auto 16px" }}>Sabores de la Tierra</Typography>
         <Box>
-          <Typography gutterBottom>Nombre de usuario: {userName}</Typography>
+          <Typography gutterBottom>Usuario: {userName}</Typography>
           <Typography gutterBottom>Fecha: {fechaYHora[0]} </Typography>
           <Typography gutterBottom>Reservación: {people} {people === 1 ? "persona" : "personas"}</Typography>
           <Typography gutterBottom>Hora: {fechaYHora[1]}hs</Typography>
@@ -56,4 +78,4 @@ const Ticket2 = ({ reserva, userName }) => {
   );
 }
 
-export default Ticket2;
+export default Ticket;
